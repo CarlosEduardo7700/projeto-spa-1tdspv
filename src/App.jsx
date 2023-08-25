1a3fb4837da372f6c810287ed97b8701f0bb0b78
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-08-24 22:51:41 -0300

    Criação do cabeçalho, da seção e do rodapé.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg"
+import Cabecalho from "./components/Cabecalho";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  let reactLogoTextoAlt = "Logo do react";
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Cabecalho/>
+
+        <section>
+          <div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, blanditiis eius animi autem dolorum alias.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, blanditiis eius animi autem dolorum alias.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, blanditiis eius animi autem dolorum alias.</p>
+          
+            <img src={reactLogo} alt={reactLogoTextoAlt} />
+          </div>
+        </section>
+        
+        <footer>
+          <div>
+            <ul>
+              <li><a href="#">GitHUb</a></li>
+              <li><a href="#">X</a></li>
+              <li><a href="#">Twiter</a></li>
+            </ul>
+          </div>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,15 @@
+export default function Cabecalho() {
+
+    return (
+        <>
+            <header>
+                <h1>Vite + React / Coded By rm552164</h1>
+                <ul>
+                <li><a href="#">Item-1</a></li>
+                <li><a href="#">Item-2</a></li>
+                <li><a href="#">Item-3</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
