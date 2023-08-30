38322a1673d7e43b5b58bfc42043227ba8674bea
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-08-29 22:57:35 -0300

    Criação de routes.

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx';
+import Produtos from './components/Produtos.jsx';
+import EditarProduto from './routes/EditarProduto.jsx';
+import Error from './components/Error.jsx';
+
+const hauter = createBrowserRouter([
+  {path:"/", element: <App/>, errorElement:<Error/>, 
+  children: [
+    {path: "/", element: <Home/>},
+    {path: "/produtos", element: <Produtos/>},
+    {path: "/produtos/editar/:id", element: <EditarProduto/>},
+  ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={hauter}/>
   </React.StrictMode>,
 )
