d459745524fa1d5c795508fb3aaf18509d294ca2
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-09-14 22:10:45 -0300

    Estrutura do ExcluirProdutos add.

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,7 +1,21 @@
-import React from "react"
-
 export default function ExcluirProdutos(){
    return (
-    <div>ExcluirProdutos</div>
+      <>
+         <h1>Excluir Podutos</h1>
+         <div>
+            <section>
+               <h2>Produto selecionado para exclusão</h2>
+               <h3>Você tem certeza de que deseja excluir esse produto?</h3>
+               <figure>
+                  <img src="" alt="" />
+                  <figcaption>
+                     Nome - R$ Preço
+                  </figcaption>
+               </figure>
+               <button>Excluir</button>
+               <button>Cancelar</button>
+            </section>
+         </div>
+      </>
    )
 }
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -33,42 +33,42 @@ export default function Produtos() {
       <h1>Lista de Produtos</h1>
 
 
-    <div>
-      <button onClick={() => setCount(count + 1)}>CLICK - {count}</button>
-    </div>
-
-    <div>
-      <table className={classes.tableStyle}>
-        <thead>
-          <tr className={classes.tableStyle.tableHeaderStyle}>
-            <th className={classes.tableHeaderStyle}>ID</th>
-            <th className={classes.tableHeaderStyle}>NOME</th>
-            <th className={classes.tableHeaderStyle}>DESCRIÇÃO</th>
-            <th className={classes.tableHeaderStyle}>PREÇO</th>
-            <th className={classes.tableHeaderStyle}>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-            {exemplo.map( (produto, index) => (
-              <tr key={index} className={classes.tableLineStyle}>
-                <td className={classes.tableDataStyle}>{produto.id}</td>
-                <td className={classes.tableDataStyle}>{produto.nome}</td>
-                <td className={classes.tableDataStyle}>{produto.desc}</td>
-                <td className={classes.tableDataStyle}>{produto.preco}</td>
-                <td className={classes.tableDataStyle}>
-                  <Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | 
-                  <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="4" className={classes.tableDataStyle}>Total de Produtos: {ListaProdutos.length}</td>
-            </tr>
-          </tfoot>
-        </table>
+      <div>
+        <button onClick={() => setCount(count + 1)}>CLICK - {count}</button>
       </div>
+
+      <div>
+        <table className={classes.tableStyle}>
+          <thead>
+            <tr className={classes.tableStyle.tableHeaderStyle}>
+              <th className={classes.tableHeaderStyle}>ID</th>
+              <th className={classes.tableHeaderStyle}>NOME</th>
+              <th className={classes.tableHeaderStyle}>DESCRIÇÃO</th>
+              <th className={classes.tableHeaderStyle}>PREÇO</th>
+              <th className={classes.tableHeaderStyle}>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+              {exemplo.map( (produto, index) => (
+                <tr key={index} className={classes.tableLineStyle}>
+                  <td className={classes.tableDataStyle}>{produto.id}</td>
+                  <td className={classes.tableDataStyle}>{produto.nome}</td>
+                  <td className={classes.tableDataStyle}>{produto.desc}</td>
+                  <td className={classes.tableDataStyle}>{produto.preco}</td>
+                  <td className={classes.tableDataStyle}>
+                    <Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | 
+                    <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan="4" className={classes.tableDataStyle}>Total de Produtos: {ListaProdutos.length}</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
     </div>
   );
 }
